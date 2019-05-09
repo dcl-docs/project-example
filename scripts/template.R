d844e27f5c5412c6a0c874329b90586694731d6e
Bill Behrman <behrman@stanford.edu>
2019-05-09 12:51:28 -0700

    Update

--- scripts/template.R
+++ scripts/template.R
@@ -1,14 +1,15 @@
-# [brief description of what the script does]
+# [Brief description of what the script does]
 
-# Source: [where you got the data from]
+# Source: [Where you got the data from]
 
-# Author: [your name]
-# Version: [date]
+# Author: [Your name]
+# Version: [Date script was last modified in YYYY-MM-DD format]
 
 # Libraries
+library(tidyverse)
 
 # Parameters
 
 #===============================================================================
 
-# [code]
+# [Code]
